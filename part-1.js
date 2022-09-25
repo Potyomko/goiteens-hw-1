376eca86ea3a5085f8540ab82414769a49feddd3
Potyomko Veronika <vnpotomko@gmail.com>
2022-09-25 11:28:51 +0300

    Додає дз

---
+++ part-1.js
@@ -0,0 +1,64 @@
+// Пункт 1
+const str = "Helo";
+console.log(str);
+console.log(typeof str);
+
+const num = 123;
+console.log(num);
+console.log(typeof num);
+
+const flag = true;
+console.log(flag);
+console.log(typeof flag);
+
+const txt = "true";
+console.log(txt);
+console.log(typeof txt);
+
+// Пункт 2
+
+const year = 2009;
+console.log(year);
+console.log(typeof year);
+
+const username = false;
+console.log(username);
+console.log(typeof username);
+
+const day = 11;
+console.log(day);
+console.log(typeof day);
+
+const month = "August"
+console.log(month);
+console.log(typeof month);
+
+const hour = 9;
+console.log(hour);
+console.log(typeof hour);
+
+let string = "Laptop";
+console.log(string);
+console.log(typeof string);
+
+let number = 13;
+console.log(number);
+console.log(typeof number);
+
+let numbers = "13";
+console.log(numbers);
+console.log(typeof numbers);
+
+let text = "false";
+console.log(text);
+console.log(typeof text);
+
+let date = 2009;
+console.log(date);
+console.log(typeof date);
+
+// Пункт 1.1
+
+const usernames = prompt ('Введіть своє імʼя');
+console.log(usernames);
+console.log(typeof usernames);
---
+++ part-2.js
@@ -0,0 +1,78 @@
+// Пункт 2
+const myage = 13;
+console.log(myage);
+
+const kateage = 13;
+console.log(kateage);
+
+alert (kateage+myage);
+
+// Пункт 3
+
+const a = 13;
+console.log(a);
+console.log(typeof a);
+
+const b = 8;
+console.log(b);
+console.log(typeof b);
+
+alert (a*b);
+
+// Пункт 4
+
+let a1 = 5 + 3;
+console.log(a1);
+
+let a2 = 5-3;
+console.log(a2);
+
+let a3 = 5*3;
+console.log(a3);
+
+let a4 = 5/3;
+console.log(a4);
+
+// Пункт 5
+
+let a6 = 5 % 3;
+console.log(a6);
+alert (a6);
+
+let a7 = 3 % 5;
+console.log(a7);
+alert (a7);
+
+let a8 = 5 + '3';
+console.log(a8);
+alert (a8);
+ 
+let a9 = '5' - 3;
+console.log(a9);
+alert (a9);
+
+let a10 = 75 + 'кг';
+console.log(a10);
+alert (a10);
+
+// Пункт 6
+
+const height = 23;
+console.log(height);
+
+const weight = 10;
+console.log(weight);
+
+const s = height*weight;
+console.log(s);
+
+// Пункт 7
+
+const heightC = 10;
+console.log(heightC);
+
+const dC = 4;
+console.log(dC);
+
+const v = heightC*dC;
+console.log(v);
